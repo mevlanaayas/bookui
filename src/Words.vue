ba153fc03d8b5d2847d5639ebdf8ed546efe2ff4
mevlanaayas <mevlanaayas@gmail.com>
2019-06-25 15:57:22 +0300

    added: listing words

--- src/Words.vue
+++ src/Words.vue
@@ -1,40 +1,86 @@
 <template>
     <div>
-        <p> <Button color="blue" icon="h-icon-plus" @click="add(datas)">Add a line</Button></p>
-        <Table :datas="datas" stripe checkbox>
-            <TableItem title="Index" prop="$index"></TableItem>
-            <TableItem title="Serial" prop="$serial"></TableItem>
-            <TableItem title="Name" prop="name" sort="auto"></TableItem>
-            <TableItem title="Age" prop="age"></TableItem>
-            <TableItem title="Address" align="center" prop="address"></TableItem>
-            <TableItem title="Operate">
+
+        <p>
+            <Button color="blue" icon="h-icon-plus" @click="openWordModal = true">Add New Word</Button>
+        </p>
+        <Modal v-model="openWordModal">
+            <CreateWord @close="openWordModal=false"></CreateWord>
+        </Modal>
+        <Modal v-model="openWordDetailModal" v-bind="params">
+            <div slot="header">Vue</div>
+            <div :style="{'height': height ? '800px' : 'auto'}">
+                <div>This is a custom vue popup</div>
+                <div><Select dict="simple" v-width="160"></Select></div>
+            </div>
+            <div slot="footer">
+                <button class="h-btn" @click="openWordDetailModal = false">Close</button>
+                <button class="h-btn h-btn-primary" @click="$Message.success('hımm')">Dunno</button>
+            </div>
+        </Modal>
+
+        <h-table @trclick="trclick" :datas="words" stripe >
+            <TableItem title="Id" align="center" prop="ID" sort="true"></TableItem>
+            <TableItem title="Name" align="center" prop="Word" sort="true"></TableItem>
+            <TableItem title="Name" align="center" prop="Sentence" sort="true"></TableItem>
+            <TableItem title="Name" align="center" prop="Translate" sort="true"></TableItem>
+            <TableItem title="Username" align="center" prop="CreatedUser" sort="true"></TableItem>
+            <TableItem title="Created Date" align="center" sort="true">
                 <template slot-scope="{data}">
-                    {{data.name}}: Custom display
+                    {{data.CreatedAt | formatDateTime}}
                 </template>
             </TableItem>
-            <div slot="empty">Custom reminder: no data at this time</div>
-        </Table>
+            <TableItem title="Last Modified" align="center" sort="auto">
+                <template slot-scope="{data}">
+                    {{data.UpdatedAt | formatDateTime}}
+                </template>
+            </TableItem>
+            <TableItem title="Related Book" align="center" sort="true">
+                <template slot-scope="{data}">
+                    {{data.BookId}}
+                </template>
+            </TableItem>
+            <div slot="empty">There is no book saved. ATM</div>
+        </h-table>
     </div>
 </template>
 
 <script>
+    import Api from './api'
+    import CreateWord from './CreateWord'
+
     export default {
+        components: {
+            CreateWord
+        },
         data() {
             return {
-                datas: [
-                    { id: 5, name: 'test 5', age: 12, address: 'Shanghai' },
-                    { id: 6, name: 'test 6', age: 13, address: 'Shanghai' },
-                    { id: 7, name: 'test 7', age: 14, address: 'Shanghai' },
-                    { id: 5, name: 'test 5', age: 15, address: 'Shanghai' },
-                    { id: 6, name: 'test 6', age: 16, address: 'Shanghai' },
-                    { id: 7, name: 'test 7', age: 17, address: 'Shanghai' }
-                ]
+                params: {
+                    closeOnMask: true,
+                    hasMask: true,
+                },
+                openWordDetailModal: false,
+                openWordModal: false,
+                words: []
             };
         },
         methods: {
-            add(datas) {
-                datas.push({ id: 7, name: 'Add to', age: 12, address: 'Then added' });
+            trclick(data, event, index) {
+                this.openWordDetailModal = true;
             }
+        },
+        created() {
+            Api.Words({'params': {'username': 'mevlana'}}).then(response => {
+                if (response.status === 200) {
+                    this.$Message.success("Successfully retrieved words");
+                    this.words = response.data.data;
+                    console.log("Words: ", this.books)
+
+                } else {
+                    this.$Message.error("Error getting words")
+                }
+            })
         }
+
     };
 </script>
